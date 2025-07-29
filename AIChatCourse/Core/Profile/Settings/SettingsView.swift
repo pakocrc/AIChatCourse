acc829b98061b8b3b726eb6a8483181d2e74434b
Francisco Cordoba <pako.cordoba10@gmail.com>
2025-07-29 15:37:01 -0600

    Fix sign out animation

--- AIChatCourse/Core/Profile/Settings/SettingsView.swift
+++ AIChatCourse/Core/Profile/Settings/SettingsView.swift
@@ -10,33 +10,33 @@ import SwiftUI
 struct SettingsView: View {
     @Environment(AppState.self) private var appState
     @Environment(\.dismiss) private var dismiss
-
+    
     var body: some View {
         NavigationStack {
             List {
                 Text("Settings")
-
+                
                 Button {
                     signOut()
                 } label: {
                     Text(String(localized: "Sign Out"))
                 }
             }
-
-                .navigationTitle("Settings")
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button {
-                            dismiss()
-                        } label: {
-                            Image(systemName: "xmark")
-                        }
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
                     }
                 }
+            }
         }
     }
-
+    
     private func signOut() {
+        dismiss()
         appState.updateViewState(showTabBar: false)
     }
 }
