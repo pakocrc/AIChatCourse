ae12a0240059246f70a9c54113a14cd816283fd6
Francisco Cordoba <pako.cordoba10@gmail.com>
2025-07-29 20:39:45 -0600

    Welcome view redesigned. Localizable strings included. Constants included

--- AIChatCourse/Core/Onboarding/WelcomeView.swift
+++ AIChatCourse/Core/Onboarding/WelcomeView.swift
@@ -8,23 +8,69 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State var imageUrl = Constants.randomImageUrl
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Welcome")
-                    .frame(maxHeight: .infinity)
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get started")
-                        .callToAction()
-                }
-                .padding()
+                ImageLoaderView(imageUrlString: imageUrl)
+                    .ignoresSafeArea()
+                
+                middleSection
+                
+                bottomSection
+            }
+        }
+    }
+    
+    private var middleSection: some View {
+        Text("AI Chat")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding(.top)
+    }
+    
+    private var bottomSection: some View {
+        VStack(spacing: 20) {
+            
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get started")
+                    .callToAction()
+            }
+            .padding()
+            
+            Text("Already have an account? Sign in")
+                .font(.headline)
+                .fontWeight(.regular)
+                .underline()
+            
+            HStack(spacing: 10) {
+                Link("Terms of Service", destination: URL(string: Constants.termsOfServiceUrl)!)
+                    .font(.callout)
+                    .fontWeight(.medium)
+                
+                Circle()
+                    .fill(.accent)
+                    .frame(width: 4, height: 4)
+                
+                Link("Privacy Policy", destination: URL(string: Constants.privacyPolicyUrl)!)
+                    .font(.callout)
+                    .fontWeight(.medium)
             }
         }
     }
 }
 
-#Preview {
+#Preview("Light") {
     WelcomeView()
+        .environment(AppState())
+        .colorScheme(.light)
+}
+
+#Preview("Dark") {
+    WelcomeView()
+        .environment(AppState())
+        .colorScheme(.dark)
 }
---
+++ AIChatCourse/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AIChatCourse
+//
+//  Created by Francisco Cordoba on 29/7/25.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImageUrl = "https://picsum.photos/500/600"
+    static let termsOfServiceUrl = "https://www.google.com"
+    static let privacyPolicyUrl = "https://www.google.com"
+}
